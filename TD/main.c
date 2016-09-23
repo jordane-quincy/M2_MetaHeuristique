201e913d2919aa6278d98fbba6ae935f7ea83275
JbRezidu <duriez.jeanbaptiste@gmail.com>
2016-09-23 14:45:27 +0200

    END

--- TD/main.c
+++ TD/main.c
@@ -42,6 +42,10 @@ int* getTableauOrdonne (Mkp *mkp, int* ordre) {
         tabOrdonne[i].ratio = coefObj/poidsObj;
     }
     qsort((void *)(tabOrdonne + 1), (size_t) mkp->n, (size_t)sizeof(ObjRatio), compareRatio);
+
+    for (i = 1; i <= 8; i++) {
+        printf("Obj index : %d, obj ratio : %lf\n", tabOrdonne[i].indexObj, tabOrdonne[i].ratio);
+    }
     for (j = 1; j<= mkp->n; j++) {
         ordre[j] = tabOrdonne[j].indexObj;
     }
@@ -52,15 +56,26 @@ int main(int argc, char *argv[]) {
 	Mkp *mkp;
 	Solution *s;
 	int *ordre;
+	int i;
 
 	if(argc != 2) {
 		printf("Usage: programme fichier\n");
 		exit(0);
 	}
 	mkp = load_mkp(argv[1]);
-	printf("%d", mkp->n);
+	s = alloc_sol(mkp);
+	init_sol(s, mkp);
 	ordre = (int *)malloc(sizeof (int) * (mkp->n + 1));
     ordre = getTableauOrdonne(mkp, ordre);
+    for (i = 1; i <= mkp->n; i++) {
+        if (Is_Add_F(mkp, s, ordre[i]) == 1) {
+            Add(mkp, s, ordre[i]);
+        }
+    }
+
+    printf("obj = %d\n", s->objValue);
+
+
 	/*save_mkp(mkp, "verif.txt");
 
 	s = alloc_sol(mkp);
